4e4ab423c89a44a2ccdfa172cd9147484fa13772
Clarence Y Cheng <cyucheng28@gmail.com>
2014-05-19 18:28:08 -0700

    Clean up mohca_secondary_structure folder

--- mohca_secondary_structure/combine_diagonal.m
+++ mohca_secondary_structure/combine_diagonal.m
@@ -36,55 +36,5 @@ for i = 1:size(D_diag, 1)-10
     end
 end
 
-    %mohcaplot( D_comb/3, seqpos, ligpos, 'Combine diagonals', '', '', secstr );
 end
 
-
-%% Vertical comparison 
-% for h = 1:length(thres)
-%     for i = 1:size(D_diag, 1)-10
-%         for j = 1:size(D_diag, 2)-10
-%             if j > i
-%                 if D_diag(i,j) > 0
-%                     tot = D_diag(i,j);
-%                     for n = 8:10
-%                         if D_diag(i,j+n) > D_diag(i,j)*thres(h)
-%                             tot = [tot D_diag(i,j+n)];
-%                             size(tot);
-%                         end
-%                     end
-%                     if length(tot) > 1
-%                         D_comb(i,j+shift) = mean(tot);
-%                     else
-%                         if DEL == 1
-%                             D_comb(i,j+shift) = 0;
-%                         elseif DEL == 0
-%                             D_comb(i,j+shift) = D_diag(i,j+shift);
-%                         end
-%                     end
-%                 end
-%             elseif i > j
-%                 if D_diag(i,j) > 0
-%                     tot = D_diag(i,j);
-%                     for n = 8:10
-%                         if D_diag(i+n,j) > D_diag(i,j)*thres(h)
-%                             tot = [tot D_diag(i+n,j)];
-%                             size(tot);
-%                         end
-%                     end
-%                     if length(tot) > 1
-%                         D_comb(i+shift,j) = mean(tot);
-%                     else
-%                         if DEL == 1
-%                             D_comb(i+shift,j) = 0;
-%                         elseif DEL == 0
-%                             D_comb(i+shift,j) = D_diag(i+shift,j);
-%                         end
-%                     end
-%                 end
-%             end
-%         end
-%     end
-% 
-%     mohcaplot( D_comb/3, seqpos, ligpos, 'Combine diagonals', '', '', secstr );
-% end
