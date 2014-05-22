7898e910ca9a79bb6e2788edd79b917b65b14504
Clarence Y Cheng <cyucheng28@gmail.com>
2014-05-22 13:37:50 -0700

    Delete some benchmarking files, leaving P4P6 and FN riboswitch as example data/analyses

--- benchmarks/mohca_secondary_structure/broaden.m
+++
@@ -1,9 +0,0 @@
-function res = broaden(data, n)
-res = data;
-struct = zeros(3);
-struct(:, 2) = 1;
-struct(2, :) = 1;
-for i=1:n
-    res = imdilate(res, struct);
-end
-end
--- benchmarks/mohca_secondary_structure/mohca_symmetric.m
+++
@@ -1,9 +0,0 @@
-function res = mohca_symmetric(data)
-res = data;
-for i=1:size(data, 1)
-    for j=i:size(data, 2)
-        res(j,i) = res(i,j);
-    end
-end
-
-end
