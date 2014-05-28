f6029c3bfe3f65a4971afefdc09db08e3b21781c
T47 <t47@stanford.edu>
2014-05-28 11:18:44 -0700

    advanced output of MOHCA runs
    Return variables (structure_mohca, bpp_2D_mohca, d_final, structure_na)
    after mohca_RNAstructure, including all sub steps of d_final. Also do
    both MOHCA and NODATA run, with area_pred overlays.

--- mohca_secondary_structure/mohca_RNAstructure.m
+++ mohca_secondary_structure/mohca_RNAstructure.m
@@ -1,4 +1,4 @@
-function mohca_RNAstructure(name, rdatname, structure, outprefix, pseudoknots, nboot, zsthreshold, docombine, dobroaden, binarize)
+function [structure_mohca, bpp_2D_mohca, d_out, structure_na] = mohca_RNAstructure(name, rdatname, structure, outprefix, pseudoknots, nboot, zsthreshold, docombine, dobroaden, binarize)
 
 if ~exist('zsthreshold', 'var')
     zsthreshold = 1.2;
@@ -27,14 +27,21 @@ sequence = rdat.sequence;
 xpos = strfind(sequence, 'X');
 sequence =  sequence(1:xpos(2)-1);
 sequence = strrep(sequence, 'X', '');
+
 d = mohca_symmetric(rdat.reactivity);
 d_filtered = diagonal_filter(d, zsthreshold);
 d_final = d_filtered;
 if( docombine )
-    d_final = combine_diagonal(d_final, 0, 0.025, 2);
+    d_diag_combined = combine_diagonal(d_final, 0, 0.025, 2);
+    d_final = d_diag_combined;
+else
+    d_diag_combined = [];
 end
 if( dobroaden )
-    d_final = broaden(d_final, 1);
+    d_broadened = broaden(d_final, 1);
+    d_final = d_broadened;
+else
+    d_broadened = [];
 end
 if( binarize )
     d_final(d_final > 0) = 1;
@@ -42,7 +49,7 @@ end
 
 seqstr = {sequence, structure};
 
-mohcaplot_biers(d_final, '', '', [outprefix, name, ': Secondary Structure Bonuses from MOHCA'], '', [outprefix, name, '_bonuses.eps'], seqstr );
+mohcaplot_biers(d_final, '', '', [outprefix, name, ': Secondary Structure Bonuses from MOHCA'], '', [outprefix, name, '_bonuses.eps'], seqstr, 1 );
 
 if( zsthreshold < 0 )
     d_final = [];
@@ -50,9 +57,23 @@ else
     d_final = -d_final(1:length(sequence),1:length(sequence));
 end
 
-[structure_mohca, bpp_2D] = rna_structure( sequence, [], 0, [], d_final, nboot, pseudoknots );
+[structure_mohca, bpp_2D_mohca] = rna_structure( sequence, [], 0, [], d_final, nboot, pseudoknots );
+structure_na = rna_structure( sequence, [], 0, [], [], 0, pseudoknots );
 fprintf(1, '%s\n', structure_mohca);
 fprintf(1, '%s\n', structure);
-output_varna_html( [outprefix, name, '.html'], sequence, structure_mohca, structure, structure_mohca, rdat.offset, [], [], [], bpp_2D );
+if isempty(structure); 
+    str1 = structure_mohca; 
+    str2 = structure_na;
+else
+    str1 = structure;
+    str2 = structure;
+end;
 
+output_varna_html( [outprefix, name, '_MOHCA.html'], sequence, structure_mohca, str1, structure_mohca, rdat.offset, [], [], [], bpp_2D_mohca );
+output_varna_html( [outprefix, name, '_NA.html'], sequence, structure_na, str2, structure_na, rdat.offset, [], [], [], [] );
+
+mohcaplot_biers(-d_final, '', '', [outprefix, name, ': NoData Overlay'], '', [outprefix, name, '_NA_overlay.eps'], {sequence, structure_na}, 2 );
+mohcaplot_biers(-d_final, '', '', [outprefix, name, ': MOHCA Overlay'], '', [outprefix, name, '_MOHCA_overlay.eps'], {sequence, structure_mohca}, 2 );
+
+d_out = { d_final, d_filtered, d_diag_combined, d_broadened };
 end
--- mohca_secondary_structure/mohcaplot_biers.m
+++ mohca_secondary_structure/mohcaplot_biers.m
@@ -1,4 +1,4 @@
-function mohcaplot( D, seqpos, ligpos, titl, ticksize, save_path, secstr )
+function mohcaplot_biers( D, seqpos, ligpos, titl, ticksize, save_path, secstr, c )
 
 % Plots 2D maps in MOHCA style
 %
@@ -10,22 +10,24 @@ function mohcaplot( D, seqpos, ligpos, titl, ticksize, save_path, secstr )
 %       ticksize  opt  = font size of tick labels (default 25, enter '' for default) 
 %       save_path opt  = path to save file (including filename) (if none, enter '') 
 %       secstr    opt  = cell array with {sequence, secstr, offset, data_types, numlanes} 
+%       c         opt  = colorcode (1 for jet, 2 for gray)
 %
 % Clarence Cheng, 2014
 %
 
 if ~exist( 'seqpos', 'var' ) || isempty( seqpos ); seqpos = 1:size(D,2); end
 if ~exist( 'ligpos', 'var' ) || isempty( ligpos ); ligpos = 1:size(D,1); end
-if ~exist( 'ticksize', 'var' ) || isempty( ticksize ); ticksize = 25; end
+if ~exist( 'ticksize', 'var' ) || isempty( ticksize ); ticksize = 12; end
 if ~exist( 'titl', 'var' ); titl = ''; end
+if ~exist( 'c', 'var') || isempty( c ); c = 1; end;
 % if ~exist( 'contours', 'var' ); contours = ''; end
 
 % Make plot
 figure;
-set(gcf, 'PaperPositionMode','auto','color','white');
-image( seqpos, ligpos, 50 * D' );
+set(gcf, 'PaperPositionMode', 'Manual','PaperOrientation', 'Landscape','PaperPosition', [-0.65 0.15 12 8],'color','white','Position', [0 0 800 600]);
+image( seqpos, ligpos, 50 * D' ); hold on;
 axis image;
-colormap( jet );
+if c == 1; colormap( jet); else colormap( 1-gray ); end;
 
 % Label x and y axes
 gp = find( mod(seqpos,10) == 0 );
@@ -33,9 +35,9 @@ set(gca,'xtick',seqpos(gp) )
 gp = find( mod(ligpos,10) == 0 );
 set(gca,'ytick',ligpos(gp) )
 set(gca,'TickDir','out');
-set(gca,'xgrid','on','ygrid','on','fonts',ticksize,'fontw','bold');
-xlabel( 'Reverse transcription stop position [5'']','fontsize',25,'fontweight','bold' );
-ylabel( 'Cleaved and ligated position [3'']','fontsize',25,'fontweight','bold' );
+set(gca,'xgrid','on','ygrid','on','fonts',ticksize);
+xlabel( 'Reverse transcription stop position [5'']','fontsize',15,'fontweight','bold' );
+ylabel( 'Cleaved and ligated position [3'']','fontsize',15,'fontweight','bold' );
 hold on;
 
 % Add title
@@ -51,7 +53,7 @@ y=xLabelPosition(2) - 7;
 XTick=str2num(xticklabel)+1;
 y=repmat(y,length(XTick),1);
 fs = get(gca,'fonts');
-hText=text(XTick,y,xticklabel,'fonts',ticksize,'fontw','bold');
+hText=text(XTick,y,xticklabel,'fonts',ticksize);
 set(hText,'Rotation',90,'HorizontalAlignment','right');
 xlab=get(gca,'XLabel');
 set(xlab,'Position',get(xlab,'Position') + [0 7 0]);
@@ -63,7 +65,7 @@ set(hc,'YTick',[0.5 hcm-0.5]);
 set(hc,'YTickLabel',{'0.0','1.0'});
 hcp = get(hc,'pos');
 pos = get(gca,'pos');
-set(hc,'position',[hcp(1)*0.92 hcp(2) hcp(3)*0.5 pos(4)*0.25],'fonts',25,'fontw','bold');
+%set(hc,'position',[hcp(1)*0.92 hcp(2) hcp(3)*0.5 pos(4)*0.25],'fonts',15,'fontw','bold');
 
 % Overlay secondary structure
 if exist( 'secstr', 'var' )
@@ -73,7 +75,7 @@ if exist( 'secstr', 'var' )
     for i = 1:length(secstr{1})
         data_types{i} = num2str(i);
     end
-    area_pred = generate_area_pred(seq, str, 0, data_types, length(secstr{1}));
+    area_pred = triu(generate_area_pred(seq, str, 0, data_types, length(secstr{1})));
         % in future, use sequence and secstruct from rdat and crop to correct size
     [x_pred, y_pred] = find(area_pred);
     x_pred = x_pred - 1 + seqpos(1);
