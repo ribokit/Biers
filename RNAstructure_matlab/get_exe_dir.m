bf809a3f0919fdec87855ce0b3e1a89c30e2c97c
Andy Watkins <amw579@stanford.edu>
2017-05-24 10:12:20 -0700

    for-->parfor on das lab imac. update EXE_DIR in get_exe_dir.m

--- RNAstructure_matlab/get_exe_dir.m
+++ RNAstructure_matlab/get_exe_dir.m
@@ -2,4 +2,6 @@ function EXE_DIR = get_exe_dir()
 
 %EXE_DIR =   '/home/tsuname/Documents/lab/rhiju/biers/external/RNAstructure/exe/';
 %EXE_DIR = '/MATLAB_Code/external/RNAstructure/exe/';
-EXE_DIR = '/MATLAB_Code/RNAstructure/exe/';
+%EXE_DIR = '/MATLAB_Code/RNAstructure/exe/';
+EXE_DIR = '/Users/daslab/src/RNAstructure/exe/';
+
--- RNAstructure_matlab/run_rna_structure_with_bootstrap.m
+++ RNAstructure_matlab/run_rna_structure_with_bootstrap.m
@@ -24,7 +24,7 @@ for i = 1:NUM_BOOTSTRAP;   structure_boot{i} = ''; end
 % this is set up to work with the Matlab parallelization toolbox.
 if exist( 'matlabpool' ) && parallelization_exists()
     if matlabpool( 'size' ) == 0;    res = findResource;  matlabpool( res.ClusterSize ) ; end
-    for n = 1:NUM_BOOTSTRAP
+    parfor n = 1:NUM_BOOTSTRAP
         [structure_boot{n}, all_bpp(:,:,n) ] = main_loop( n, seq_file, temperature, experimental_offset, zscore_scaling, EX_file, SHAPE_file, shape_intercept, shape_slope, USE_VIENNA, maxdist, cmd_pk, DMS_file );
     end
 else
