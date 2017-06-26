b0483517d85533440398d0ad9bcae25b30b0e85b
Rhiju Das <rhiju@stanford.edu>
2017-06-26 16:20:43 -0700

    update RNAstructure exe/ guessing from DATAPATH to handle case where user does not unclude trailing slash

--- Scripts/get_exe_dir.m
+++ Scripts/get_exe_dir.m
@@ -11,5 +11,11 @@ if length( rna_structure_datapath ) == 0 | ~exist( rna_structure_datapath, 'dir'
     error( sprintf( '\n\nYou must install RNAstructure and set the DATAPATH variable in your .bashrc.\nIf you did that, make sure you start matlab from Terminal with %s/bin/matlab\n', matlabroot ) );
 end
 
-EXE_DIR = [ fileparts(fileparts(rna_structure_datapath)), '/exe/' ] ;
+[EXE_DIR,datapathname] = fileparts(rna_structure_datapath);
+
+% user may have included a trailing slash in 'data_tables/':
+if length( datapathname ) == 0; [ EXE_DIR, datapathname ] = fileparts( EXE_DIR ); end;
+
+assert( strcmp( datapathname, 'data_tables' )  )
+EXE_DIR = [EXE_DIR, '/exe/'] ;
 if ~exist( EXE_DIR, 'dir' ) open( mfilename ); return; end;
